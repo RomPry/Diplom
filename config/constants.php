d23f8991dde8d6f0a12f5c9520ba289a1a6a5091
Roman <romka061189@gmail.com>
2021-02-23 02:04:28 +0300

    Add 3 files
    Add to config:
    - file with constants
    
    Add to components:
    - 2 files (select and pagination)

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+	define('DOMAIN', 'localhost/');
+	define('SCHEME', 'http://');
+	define('SITE_ROOT', 'tunguska/');
+	define('FULL_SITE_ROOT', SCHEME . DOMAIN . SITE_ROOT);
+	define('SITE_FILE_ROOT', 'C:/xampp/htdocs/');
+	define('ASSETS', '/' . SITE_ROOT . 'assets/');
+    define('IMG', ASSETS . 'img/');
+	define('CSS', ASSETS . 'css/');
+	define('JS', ASSETS . 'js/');
+    define('LIBS', ASSETS . 'libs/');
+
+?>
